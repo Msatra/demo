414149f992ec020d5a6130bf7d0a9b504fae2d4c
msatra amir <msatra.amir@gmail.com>
2015-01-22 23:09:18 -0300

    2 eme modificartion

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -15,7 +15,10 @@ public class DemoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO World");
+        if (args.length !=1){
+            System.out.println("wrong args");
+
+        System.exit(1);
     }
-    
+    System.out.println("HELLO" + args[0] + "!");
 }
